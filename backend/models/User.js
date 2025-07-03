ab2779e7bf18ffae153e132e24c6cb0648312097
nicolRB <134354240+nicolRB@users.noreply.github.com>
2025-07-03 19:53:29 -0300

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // hashed com bcrypt
+  password: { type: String, required: true },
   role: { type: String, enum: ['colaborador', 'gerente', 'diretor'], default: 'colaborador' },
 })
 
